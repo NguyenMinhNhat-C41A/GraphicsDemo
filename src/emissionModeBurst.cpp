80b289731e9dfcbddc180a2daf91f8aa08260795
NguyenMinhNhat-C41A <104690276+NguyenMinhNhat-C41A@users.noreply.github.com>
2024-04-09 20:53:45 -0300

    Burst Emittor Shape

--- src/emissionModeBurst.cpp
+++ src/emissionModeBurst.cpp
@@ -60,22 +60,42 @@ EmissionModeBurst::EmissionModeBurst(Emittor *p_pEmittor) : EmissionMode(p_pEmit
 
 void EmissionModeBurst::emit()
 {
+    Shape *emittorShape = this->m_pEmittor->getEmittorShape();
+    std::string emittorShapeType = emittorShape->getShapeType();
+
+    glm::vec2 shapeMinMaxX = emittorShape->getMinMaxX();
+    glm::vec2 shapeMinMaxY = emittorShape->getMinMaxY();
+    glm::vec2 shapeMinMaxZ = emittorShape->getMinMaxZ();
 
     int ParticlesToSpawn = RandomNumberGenerator::getRandomNumber(35, 50);
 
     for (int i = 0; i < ParticlesToSpawn; i++)
     {
-        // std::cout << "EMIMODECONT - EMIT:" << this->m_pTimer << std::endl;
-        glm::vec3 newParticlePos = glm::vec3(
-                                       RandomNumberGenerator::getRandomNumber(0, 2) / 4.0f,
-                                       RandomNumberGenerator::getRandomNumber(0, 2) / 4.0f,
-                                       RandomNumberGenerator::getRandomNumber(0, 2) / 4.0f) +
-                                   this->m_pEmittor->getOffset();
-
         glm::vec3 newDirection = glm::normalize(glm::vec3(
             RandomNumberGenerator::getRandomNumber(1, 64) * (RandomNumberGenerator::getRandomNumber(0, 1) - 0.5f) * 2.0f,
             RandomNumberGenerator::getRandomNumber(1, 64) * (RandomNumberGenerator::getRandomNumber(0, 1) - 0.5f) * 2.0f,
             RandomNumberGenerator::getRandomNumber(1, 64) * (RandomNumberGenerator::getRandomNumber(0, 1) - 0.5f) * 2.0f));
+
+        glm::vec3 newParticlePos = glm::vec3(0.0f);
+
+        if (emittorShapeType.compare("Point") == 0)
+        {
+            newParticlePos = glm::vec3(0.0f);
+        }
+
+        if (emittorShapeType.compare("Box") == 0)
+        {
+            newParticlePos = glm::vec3(
+                                 RandomNumberGenerator::getRandomDecimal(shapeMinMaxX.x, shapeMinMaxX.y),
+                                 RandomNumberGenerator::getRandomDecimal(shapeMinMaxY.x, shapeMinMaxY.y),
+                                 RandomNumberGenerator::getRandomDecimal(shapeMinMaxZ.x, shapeMinMaxZ.y)) +
+                             this->m_pEmittor->getOffset();
+        }
+
+        else if (emittorShape->getShapeType().compare("Sphere") == 0)
+        {
+            // TO BE ADDED
+        }
 
         float newLifespan = this->m_pEmittor->getParticleBaseLifespan() + static_cast<float>(RandomNumberGenerator::getRandomNumber(0, 128) / 64.0f);
 
